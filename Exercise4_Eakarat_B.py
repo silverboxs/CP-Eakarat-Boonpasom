c628f859f81cd6f102cc21c1d5054cb50b4ee052
silverboxs <95960588+silverboxs@users.noreply.github.com>
2021-12-11 20:17:12 +0700

    Add files via upload

---
+++ Exercise4_Eakarat_B.py
@@ -0,0 +1,11 @@
+FE = 69.5 # Foundation English Score
+GB = 99.5 # General Business Score
+ICS = 88  # Introduction to Computer Systems
+CP = 100  # Conputer Programming
+
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems", ICS)
+print("Computer Programming", CP)
+print("-----------------")
