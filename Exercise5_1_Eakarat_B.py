c2af7e428af47932b29a921d4fd6f91bdbde94ff
silverboxs <95960588+silverboxs@users.noreply.github.com>
2021-12-12 10:06:04 +0700

    Add files via upload

---
+++ Exercise5_1_Eakarat_B.py
@@ -0,0 +1,8 @@
+a = int(input("Please enter a number 1st  :"))
+b = int(input("Please enter a number 2nd  :"))
+
+print("--->Output<---")
+print(a,"+",b," = ",a+b)
+print(a,"-",b," = ",a-b)
+print(a,"x",b," = ",a*b)
+print(a,"/",b," = ",a/b)
