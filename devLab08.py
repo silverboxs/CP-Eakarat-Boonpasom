fa839d8f8664c207e5911c5396287a8b032e3796
silverboxs <95960588+silverboxs@users.noreply.github.com>
2022-01-25 01:03:11 +0700

    Update devLab08.py

--- devLab08.py
+++ devLab08.py
@@ -1,4 +1,21 @@
-upper, lower, dig, spec = 0, 0, 0, 0
+def check_charecter(cha):
+    chaList = ["!","@","#","$","/"]
+    for cl in chaList:
+        if cl == cha:
+            return 1
+        else:
+            return 0
+
+x = 0
+x += check_charecter("#")
+print(x)
+x += check_charecter("@")
+print(x)
+x += check_charecter("!")
+print(x)
+
+'''
+upper, lower, digi, spec = 0, 0, 0, 0
 psw = input()
 if len(psw) >= 3:
     for i in psw:
@@ -6,6 +23,12 @@ if len(psw) >= 3:
             upper += 1
         if i.islower():
             lower += 1
-
+        if i.isdigit():
+            digi += 1
+        spec += int(check_charecter(i))
+    
+    if uper >=1 or lower >= 1 or digi >= 1 or spec >=1:
+        print("Valid")
 else:
     print("Invalid")
+'''
