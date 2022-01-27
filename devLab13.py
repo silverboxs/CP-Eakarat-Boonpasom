d0dfd4caaa5a8c9859ab69ee1a404f5b5f806302
silverboxs <95960588+silverboxs@users.noreply.github.com>
2022-01-28 00:27:23 +0700

    Update devLab13.py

--- devLab13.py
+++ devLab13.py
@@ -1,6 +1,7 @@
 amount = int(input())
 productList = list()
 result = list()
+txt = ""
 
 for rows in range(amount):
     PDID = int(input())
@@ -10,3 +11,9 @@ IDinput = int(input())
 for x in range(len(productList)):
     if productList[x] == IDinput:
         result.append(x+1)
+
+print(result)
+for l in result:
+    txt = l,","
+
+print("Position:",txt)
