7731385ede59ea1869e80543d85d1841f3c4876c
silverboxs <95960588+silverboxs@users.noreply.github.com>
2022-02-15 17:13:22 +0700

    Update and rename Lecture114.py to Lecture114._Eakarat_B.py

--- Lecture114._Eakarat_B.py
+++ Lecture114._Eakarat_B.py
@@ -2,10 +2,7 @@ from msilib.schema import ComboBox
 from tkinter import *
 from tkinter import ttk
 from datetime import datetime
-from tracemalloc import start
-from forex_python.converter import CurrencyRates
 from forex_python.bitcoin import BtcConverter
-from idna import check_initial_combiner
 
 btc = BtcConverter()
 
@@ -44,17 +41,28 @@ def timeSet(e):
     return datetime(int(value_year.get()),month[value_month.get()],int(value_day.get()),10,0,0,151012)    
 
 def convert_to_BTC(e):
-    conver_btc2016 = btc.convert_to_btc_on(1000,'THB',timeSet(e))
+    conver_btc2016 = btc.convert_to_btc_on(int(value_amount.get()),'THB',timeSet(e))
+    conver_btc_last = btc.convert_btc_to_cur(conver_btc2016,'THB',)
+    #Text Format....
     decimalFormat = '{:.4f}'
-    print(decimalFormat.format(conver_btc2016))
-
+    THB_Format = '{:,.2f}'
+    txt_date = value_day.get()+'-'+value_month.get()+'-'+value_year.get()+'\n'
+    textResult = "จำนวน Bitcoin ที่คุณได้ : = "+decimalFormat.format(conver_btc2016)+" BTC"
+    #Label Result Buy
+    txt_btc_result = Label(Main, text=str(txt_date+textResult), font=('Tahoma',16,'bold'))
+    txt_btc_result.place(relx=0.15, rely=0.5)
+    #Text Format.....Now
+    text_result_now = "ปัจจุบัน Bitcoin ของคุณจะมีมูลค่า\n"+THB_Format.format(conver_btc_last)+".-บาท"
+    #Label Result Now
+    txt_btc_last = Label(Main,text=text_result_now, font=('Tahoma',16,'bold'))
+    txt_btc_last.place(relx=0.15,rely=0.7)
 
 Main = Tk()
 Main.geometry("500x500")
 Main.configure(background="#333333")
 Main.title("Bitcoin พารวย")
 
-text_display_1 = Label(Main, text='ถ้าคุณซื้อ Bitcoin ไว้ในปี',font=('Tahoma',14,'bold'),bg='#333333',fg='white')
+text_display_1 = Label(Main, text='>> ถ้าคุณซื้อ Bitcoin ในวันที่ <<',font=('Tahoma',14,'bold'),bg='#333333',fg='white')
 text_display_1.place(relx= 0.5, rely= 0.05, anchor='center')
 #date Label Text...
 text_day = Label(Main, text="DAY", font=('Tahoma',16,'bold'), bg='#333333', fg='white')
@@ -89,10 +97,10 @@ value_year.place(relx=0.62,rely=0.2)
 #Amount THB--
 txt_amount = Label(Main, text="จำนวนเงินที่ซื้อ:",font=('Tahoma',16,'bold'), bg='#333333', fg='white')
 txt_amount.place(relx=0.05,rely=0.3)
-value_amount = Entry(Main,width=20)
-value_amount.place(relx=0.4, rely=0.3)
+value_amount = Entry(Main)
+value_amount.place(relx=0.4, rely=0.3, width=200, height=30)
 
-button_submit = Button(Main, text="TEST", width=30,height=1)
+button_submit = Button(Main, text="คำนวณ", width=30,height=1)
 button_submit.bind('<Button-1>',convert_to_BTC)
 button_submit.place(relx=0.3, rely=0.4)
-Main.mainloop()                            
+Main.mainloop()  
